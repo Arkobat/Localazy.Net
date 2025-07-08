a53b73fd51cd377166a15fd74e6a10a84e76b588
Rob Scott <rob.scott@ungerboeck.com>
2025-07-08 08:40:15 -0400

    Use HTTP client factory

--- Localazy/Service/LocalazyFactory.cs
+++ Localazy/Service/LocalazyFactory.cs
@@ -4,13 +4,22 @@ namespace Localazy.Service;
 
 public class LocalazyFactory : ILocalazyFactory
 {
+    private readonly IHttpClientFactory _httpClientFactory;
+    
+    public LocalazyFactory(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    
     public ILocalazyService CreateService(string apiKey)
     {
         var config = new LocalazyConfig
         {
             ApiKey = apiKey
         };
-        var httpWrapper = new HttpWrapper(new HttpClient(), config);
+        var client = _httpClientFactory.CreateClient(nameof(LocalazyFactory));
+        client.BaseAddress = new Uri("https://api.localazy.com/");
+        var httpWrapper = new HttpWrapper(client, config);
         return new LocalazyService(httpWrapper);
     }
 }
